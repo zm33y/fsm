eb5aabcd2618107d717e61402c18d04a2ef640f1
Vasily Goldobin <vasily.goldobin@gmail.com>
2018-05-10 17:56:39 +0500

    Remove NoTransitionError

--- fsm.go
+++ fsm.go
@@ -280,11 +280,6 @@ func (f *FSM) Event(event string, args ...interface{}) error {
 		return err
 	}
 
-	if f.current == dst {
-		f.afterEventCallbacks(e)
-		return NoTransitionError{e.Err}
-	}
-
 	// Setup the transition, call it later.
 	f.transition = func() {
 		f.stateMu.Lock()
